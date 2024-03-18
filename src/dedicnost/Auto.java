841ea40a7dfd722f3d53a1a9e298c2b5bea6410b
egners <egners@gasos-ro.cz>
2024-03-18 13:12:50 +0100

    AAAAAAAAAAAAAAAAAAAAAcAAAAAA

---
+++ src/dedicnost/Auto.java
@@ -0,0 +1,22 @@
+package dedicnost;
+public class Auto {
+private int pocetSedadel = 5;
+private int pocetDveri = 5;
+private int pocetNahradnichKol = 1;
+int pocetKoni;
+int cena;
+String barva;
+
+public Auto(int pocetKoni,int cena,String barva){
+    this.barva = barva;
+    this.cena  = cena;
+    this.pocetKoni = pocetKoni;
+}
+
+
+
+
+
+
+
+}
---
+++ src/dedicnost/Main.java
@@ -0,0 +1,28 @@
+package dedicnost;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+        System.out.print("zadej te barvu auta: ");
+        String barva = sc.nextLine();
+        barva = sc.nextLine();
+
+        System.out.print("zadej te pocet koní: ");
+        int pocetKoni = sc.nextLine();
+        barva = sc.nextLine();
+
+
+
+
+
+
+
+
+
+
+    }
+}
